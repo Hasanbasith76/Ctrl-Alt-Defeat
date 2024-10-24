39dcf7b2ac820facbe55c0b46c5ce68b2f766979
COOBRAAA <dhilipbasha786@gmail.com>
2024-10-24 12:09:53 +0530

    Admin

--- User/client/src/Instructions.js
+++ User/client/src/Instructions.js
@@ -8,12 +8,12 @@ export default function Instructions(){
                 <h1>Instructions To Be Noted:</h1>
                 <p>Disable all the Desktop Notifications throught the assessment</p>
                 <ul>
-                    <li>Do not leave the camera preview</li>
-                    <li>Do not look away from the screen</li>
-                    <li>Do not leave your web browser</li>
-                    <li>Do not speak unless instructed</li>
-                    <li>Do not allow othersin the room with you</li>
-                    <li>Do not use any outside reference material</li>
+                    <li>Ensure webcam is functional and well connected.You will be monitored via webcam during the assessment</li>
+                    <li>Conduct assessment fairly. Please note that toggling between screens will be considered malpractice </li>
+                    <li>Keep your browser open.</li>
+                    <li>Speak only when requested to do so.</li>
+                    <li>Avoid allowing anyone else in the room.</li>
+                    <li>Don't utilize third-party resources.</li>
                 </ul>
                 <div className="nav-btn">
                     <a href="/Checkwindow">
